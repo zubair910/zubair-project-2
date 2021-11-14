072aab774ab92db619413445110e3169abc2c871
unknown <zubairjava123@gmail.com>
2021-11-14 18:44:45 +0500

    Work Complete

---
+++ app.js
@@ -0,0 +1,140 @@
+
+// var stdArr = ["Zubair" , 18 , "Male"]
+//   document.write("Name: " + stdArr[0])
+//   document.write("<br> AGE: " + stdArr[1])
+//   document.write("<br> GENDER: " + stdArr[2])
+
+// for(var i = 1 ; i <=10 ; i++) {
+//     document.write("4 x" + " " + i + " " + "=" + 4*i + "<br>")
+// }
+
+// var tabLength = +prompt("Table Length")
+
+// for(var i = 1 ; i <= tabLength ; i++)
+// document.write(tabLength + " x" + " " + i + " " + "=" + " " + tabLength*i + "<br>" )
+
+// var cities = ["islamabad" , "karachi" , "lahore" , "multan"]
+
+// for(var i = 0 ; i < cities.length ; i++) {
+//     document.write(cities[i] + "<br>")
+// }
+
+
+// var cities = ["islamabad" , "karachi" , "lahore" , "multan"]
+// var userInput  = prompt("Enter Items")
+// for(var i = 0 ; i < cities.length ; i++) {
+//     if(userCity === cities[i]) {
+//         alert("PAKISTAN")
+//     }
+// }
+
+
+ 
+// var std = ["Zubair <br>" , "huzaifa  <br>" , "jaffar  <br>"]
+// std[0]  = "Zubair <br>"
+// std[1]  = "huzaifa  <br>"
+// std[2]  = "jaffar  <br>"
+
+// document.write(std)
+
+
+// var bakery = ["cake" , "sandwich" , "icecream" , "biscuits"]
+
+// document.write(bakery)
+
+
+// var num = ["<br> 24"  , "<br> 25" , "<br> 26" , "<br>27"]
+// document.write(num)
+
+
+// var education = ["<br> SSC" , "<br> HSC" , "<br> BCS" , " <br> BCOM" , "<br> MS" , "<br> M.PHIL" , "<br> PhD"]
+
+// document.write(education )
+
+
+
+// var score = ["<br> Score of Zubair is 320.Percentage: 64% " , " <br> Score of Huzaifa is 230.Percentage: 46% " , "<br> Score of Jaffar is 480.Percentage: 96% "]
+
+
+// document.write(score)
+
+// var number = ["<br> score of std:320 , 230 ,480 ,120"]
+// document.write(number)
+// var numbers = ["<br> orderd score of std:120 , 230 ,320 ,480"]
+// document.write(numbers)
+
+
+// var name = "<br> Cities List"
+
+// document.write( name)
+
+// var cities = ["<br> karachi" , " lahore" , " islamabad" , " multan"]
+
+// document.write(cities)
+
+// var text = ["<br> This" , " is" , "my" , "cat"]
+
+// document.write(text)
+
+// var computer = ["<br> Devices:" , "<br>keyboard" , "mouse" , "printer" , "monitor"]
+
+// document.write(computer)
+
+// var computer = ["<br> Devices:" , "<br>keyboard" , "<br>mouse" , "<br>printer" , "<br>monitor"]
+
+// document.write(computer)
+
+
+// var matrix = ["<br> 0" , 1 , 2 , 3 ]
+
+// document.write( matrix)
+
+// var numeric = ["<br> 0" , "<br> 1" , "<br> 2" , "<br> 3"  , "<br> 4"  , "<br> 5"  , "<br> 6"  , "<br> 7"  , "<br> 8" , "<br> 9" , "<br> 10"]
+
+// document.write( numeric)
+
+// var tableNumber = +prompt("Table Number...")
+// var tabLength = +prompt("Table Length...")
+
+// document.write("Table Num : " + tableNumber + " " + "table Len: " +tabLength + "<br>" )
+ 
+// for(var i = 1 ; i <= tabLength ; i++){
+//     document.write(tableNumber + " x" + " " + i +" " + "=" + " " + tableNumber*i + "<br>" )
+// }
+
+
+// var  fruits = ["<br> Element index 0 is apple" , "<br> Element index 1 is orange" , "<br> Element index 2 is mango" , "<br> Element index 3 is graps"]
+
+// document.write(fruits)
+
+
+
+
+// var counting = ["<br> 0" , "<br> 1" , "<br> 2" , "<br> 3"  , "<br> 4"  , "<br> 5"  , "<br> 6"  , "<br> 7"  , "<br> 8" , "<br> 9" , "<br> 10" , "<br> 11" , "<br> 12" , "<br> 13" , "<br> 14" , "<br> 15"]
+
+// document.write( counting)
+
+
+
+// var abcBakery = ["cake" , "sandwich" , "icecream" , "biscuits"]
+// var userInput = prompt("Enter Your items")
+
+// var isMatch = "no"
+
+// for(var i = 0 ; i < abcBakery.length ; i++){
+
+//         if(userInput === "biscuits" ){
+//             alert("best cookie")
+//             isMatch = "yes"
+//             break;
+//         }
+//         else if(userInput === "sandwich"){
+
+//         }
+
+
+// }
+
+// if(isMatch == "no"){
+//     alert("WELCOME...")
+// }
